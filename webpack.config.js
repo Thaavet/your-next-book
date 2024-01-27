7aa106e189c101d28a0f3134cd03dfc45dc1cd7d
Thaavet <148262279+Thaavet@users.noreply.github.com>
2024-01-27 13:25:52 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'production',
@@ -10,33 +11,27 @@ module.exports = {
     },
     module: {
         rules: [
-            
-
-            {
-                test:    /\.css$, /,
-                
-                use: {
-                    loader: 
-                    "style-loader",
-                    'css-loader'
-                   , 
-                }
-            },
-
-            {
-              test: /\.js%/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env'],
-                },
+          {
+            test: /\.css$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader'
+            ]
+          },
+          {
+            test: /\.js%/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
               },
             },
-           
+          },
         ]
-    },
+      },
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             title: 'Your next book',
             template: './src/template.html'
